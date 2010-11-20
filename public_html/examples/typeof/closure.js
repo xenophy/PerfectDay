f4e05d3dc3dfff377e7c710f349e81d08f969e64
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-11-20 14:08:23 +0900

    作成中

---
+++ public_html/examples/typeof/closure.js
@@ -0,0 +1,61 @@
+
+Ext.onReady(function() {
+
+    /*
+
+    var f = function() {
+
+        var x = 0;
+
+        return function() {
+
+            return x++;
+
+        };
+    }
+
+    func = f();
+
+    // 0
+    ret = func();
+    console.log(ret);
+
+    // 1
+    ret = func();
+    console.log(ret);
+
+    // 2
+    ret = func();
+    console.log(ret);
+
+    */
+
+    /*
+
+    var o = {
+        foo: 'bar'
+    };
+
+    var f = function() {
+
+        console.log(arguments);
+
+        console.log(this);
+    }
+
+//    f.call(o, 123, 456);
+    f.apply(o, [123, 456]);
+    */
+
+
+
+
+
+
+
+
+
+
+});
+
+
