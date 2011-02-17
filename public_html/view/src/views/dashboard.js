30e2fa7526c349cd8dfe8ec716708907cd73d3fc
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-02-17 20:05:23 +0900

    ビューサンプル追加

---
+++ public_html/view/src/views/dashboard.js
@@ -0,0 +1,58 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * Perfect Day
+ * Copyright(c) 2011 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
+
+// {{{ dashboard
+
+Ext.define('PerfectDay.views.Viewport', {
+
+    // {{{ extend
+
+    extend: 'Ext.Viewport',
+
+    // }}}
+    // {{{ layout
+
+    layout : 'border',
+
+    // }}}
+    // {{{ initComponent
+
+    initComponent: function() {
+
+        var me = this;
+
+        Ext.apply(me, {
+            items: [{
+                region: 'north',
+                title: 'Header',
+                html: 'Header'
+            },{
+                region: 'center',
+                buttons: [{
+                    text: 'ボタン'
+                }]
+            }]
+        });
+
+        // スーパークラスメソッドコール
+        PerfectDay.views.Viewport.superclass.initComponent.apply(me, arguments);
+    }
+
+    // }}}
+
+});
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
